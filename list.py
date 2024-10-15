49ab9db3a93b297039caa6b1f46c7d5555d45e5e
Pro5507 <nhupendrakumar9162@gmail.com>
2024-10-15 19:31:07 +0530

    List

---
+++ list.py
@@ -0,0 +1,19 @@
+a= []
+print(a)
+b=["pallav", 11, 0.1465]
+print(b)
+c=[1, 2, 3, 4, 5]*3
+print(c)
+c= c[::-1]
+print(c)
+count=0
+d=[100, 200, 300, 400, 500]
+for i in d:
+    count= count+i
+print(count)
+print(len(d))
+e= [3, 12, 45, 1, 234, 2]
+e.sort()
+print(e)
+print(e[0])
+print(e[5])
